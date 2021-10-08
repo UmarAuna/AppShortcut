e6e393393233e89fddf1fe02592e66f3632de2a8
Umar Saidu Auna <umarmanofpeace@gmail.com>
2021-10-08 17:07:35 +0100

    feat: 🎸 dynamic Shortut

--- app/src/main/java/com/gidimo/appshortcut/MainActivity.kt
+++ app/src/main/java/com/gidimo/appshortcut/MainActivity.kt
@@ -7,50 +7,63 @@ import android.content.pm.ShortcutManager
 import android.graphics.drawable.Icon
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        openURL()
-        //openActivity()
+        buttons()
+    }
+
+    private fun buttons() {
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            openURL()
+        }
+
+        findViewById<Button>(R.id.button4).setOnClickListener {
+            removeShortcuts()
+        }
     }
 
     @TargetApi(25)
-    fun openURL() {
+    private fun openURL() {
         val shortcutManager = getSystemService(ShortcutManager::class.java)
-        val shortcut = ShortcutInfo.Builder(applicationContext, "id1")
-            .setShortLabel("gidimo")
-            .setLongLabel("Open gidimo website")
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.action = Intent.ACTION_VIEW
+
+        val shortcut1 = ShortcutInfo.Builder(applicationContext, "mask_up")
+            .setShortLabel("MaskUp Dynamic")
+            .setRank(0)
+            .setLongLabel("Open Maskup Dynamic")
             .setIcon(Icon.createWithResource(applicationContext, R.drawable.ic_masks))
+            .setIntent(intent)
+            .build()
+
+        val shortcut2 = ShortcutInfo.Builder(applicationContext, "web_link")
+            .setShortLabel("google Dynamic")
+            .setRank(1)
+            .setLongLabel("Open google Dynamic")
+            .setIcon(Icon.createWithResource(applicationContext, R.drawable.ic_web))
             .setIntent(
                 Intent(
                     Intent.ACTION_VIEW,
-                    Uri.parse("https://www.gidimo.c om/")
+                    Uri.parse("https://www.google.com/")
                 )
             )
             .build()
 
-        shortcutManager!!.dynamicShortcuts = listOf(shortcut)
+        shortcutManager?.dynamicShortcuts = listOf(shortcut1, shortcut2)
     }
 
-   /* @TargetApi(25)
-    fun openActivity() {
-        val sM = getSystemService(ShortcutManager::class.java)
-        val intent1 = Intent(applicationContext, MainActivity3::class.java)
-        intent1.action = Intent.ACTION_VIEW
-
-        val shortcut1 = ShortcutInfo.Builder(this, "shortcut1")
-            .setIntent(intent1)
-            .setShortLabel(getString(R.string.store))
-            .setLongLabel("Store Dynamic")
-            .setShortLabel("Store Dynamic")
-            .setDisabledMessage("Store Dynamic")
-            .setIcon(Icon.createWithResource(this, R.drawable.ic_store))
-            .build()
-
-        sM.dynamicShortcuts = listOf(shortcut1)
-    }*/
+    @TargetApi(25)
+    private fun removeShortcuts() {
+        val manager = getSystemService(ShortcutManager::class.java)
+        manager.removeAllDynamicShortcuts()
+    }
 }
