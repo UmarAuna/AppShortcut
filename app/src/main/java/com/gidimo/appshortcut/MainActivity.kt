2295a040034781503d87ea589c13f063f2363086
Umar Saidu Auna <umarmanofpeace@gmail.com>
2021-10-08 17:31:42 +0100

    refactor: 💡 fix imports

--- app/src/main/java/com/gidimo/appshortcut/MainActivity.kt
+++ app/src/main/java/com/gidimo/appshortcut/MainActivity.kt
@@ -9,7 +9,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
